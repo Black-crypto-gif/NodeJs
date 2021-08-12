f53085dcdc9f917b09d4a93e75297ae1d614b704
MELLAL Fethi <fethimellal@gmail.com>
2021-08-12 02:10:24 +0200

    Files fs

---
+++ files.js
@@ -0,0 +1,45 @@
+const  fs = require('fs');
+
+// Reading files
+
+/* fs.readFile('./fethi.txt', (err, data) => {
+    if (err) {
+        console.error(err);
+    }
+    console.log(data.toString());
+}) */
+
+//writing files : 
+
+/* fs.writeFile('./mellal.txt', 'hello fethi mellal', () => {
+    console.log(" File changed");
+}); */
+
+// Directories : 
+/* if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets',(err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('Created');
+    })
+}else{
+    fs.rmdir('./assets',(err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Removed');
+    })
+}
+ */
+
+// Deleting files :
+
+/* if (fs.existsSync('./fethi.txt')) {
+    fs.unlink('./fethi.txt', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('File deleted');
+    })
+} */
